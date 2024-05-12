157216029fede7ba7ede790f04a85ef26289c836
yezisheng01 <1364697417@qq.com>
2024-05-12 21:24:01 +0800

    first commit

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/algebra.obj"
+  "CMakeFiles/main.dir/src/algebra.obj.d"
+  "CMakeFiles/main.dir/src/main.obj"
+  "CMakeFiles/main.dir/src/main.obj.d"
+  "E:/Users/Lenovo/Desktop/yezisheng_hw1-main/bin/libmain.dll.a"
+  "E:/Users/Lenovo/Desktop/yezisheng_hw1-main/bin/main.exe"
+  "E:/Users/Lenovo/Desktop/yezisheng_hw1-main/bin/main.exe.manifest"
+  "E:/Users/Lenovo/Desktop/yezisheng_hw1-main/bin/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
